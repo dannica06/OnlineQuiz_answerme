3652b324f19c060e535e27f31eb6f1ef6471583c
dannica06 <dannica446@gmail.com>
2025-11-10 23:01:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './assets/styles.css'; // your design CSS
+import './assets/styles.css'; 
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
